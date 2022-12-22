3cd99f8dbb3b7e65336b86a858594a9786eeec9e
Your Name <you@example.com>
2022-12-22 13:19:00 +0100

    made two sections of main (setup and running) and placed the running of the Smith in them

--- main.cpp
+++ main.cpp
@@ -1,20 +1,23 @@
 
-
-
+#include "Smith.h"
+#include "Marketplace.h"
 
 
 
 int main(int argc, char *argv[])
 {
+	// Initialization:
+	Marketplace myMarketplace;
+	Smith mySmith(&myMarketplace);
+
+
+	// Running the village:
+	
+	mySmith.CalloutMetalType();
+	mySmith.HandOverMetal();
+	mySmith.CalloutMetalType();
 
 
 
-
-
-
-
-
-return 0;
-
-
+	return 0;
 }
