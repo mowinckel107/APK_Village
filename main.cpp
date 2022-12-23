a5c57479dd1393313321c051c4e87471c84080cd
Your Name <you@example.com>
2022-12-23 13:58:59 +0100

    Fixed merge errors. No real conflicts

--- main.cpp
+++ main.cpp
@@ -1,13 +1,24 @@
 
+
+#include <iostream>
+
 #include "Smith.h"
 #include "Marketplace.h"
 #include "Innkeeper.h"
 #include "BakerKing.h"
+#include "TownCrier.h"
+#include "TimeOfDay.h"
 
+
+
+void townCrierTest();
+
+static const bool isTestingDone = true; // Set to true/false to run tests or not
 
 
 int main(int argc, char *argv[])
 {
+
 	// Initialization:
 	Marketplace myMarketplace;
 	Smith mySmith(&myMarketplace);
@@ -16,17 +27,45 @@ int main(int argc, char *argv[])
 
 	BakerKing myBakerKing;
 
+	if constexpr(isTestingDone)
+	{
+		townCrierTest();
+	}
+	else
+	{
+		// Initialization:
+		TimeOfDay *timeOfDay = timeOfDay->createTimeOfDay();
+		TownCrier townCrier(timeOfDay);
+		Marketplace marketplace;
+		Smith smith(&marketplace);
 
-	// Running the village:
-	mySmith.CalloutMetalType();
-	mySmith.HandOverMetal();
-	mySmith.CalloutMetalType();
+		// Running the village:
+		mySmith.CalloutMetalType();
+		mySmith.HandOverMetal();
+		mySmith.CalloutMetalType();
 
-	myBakerKing.OrderBasicBread();
-	myBakerKing.OrderStrongBread();
+		myBakerKing.OrderBasicBread();
+		myBakerKing.OrderStrongBread();
 
-
-
+	}
 	return 0;
+}
+
+
+
+
+void townCrierTest()
+{
+
+	TimeOfDay *timeOfDay = timeOfDay->createTimeOfDay();
+	TownCrier townCrier(timeOfDay);
+
+
+	townCrier.WakeMeWhenItIsEvning();
+    std::cout << "      Villagers: \"Going to the pub!!\"" << std::endl;
+
+	townCrier.WakeMeWhenItIsNight();
+    std::cout << "      Villagers: \"Going to bed :C\"" << std::endl;
+
 }
 
